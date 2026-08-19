bf0d621297f315d636baa380b1e79b59bf0b4c0e
Sean Mitchell <sean@37signals.com>
2026-08-18 18:29:15 -0700

    Add copy JS

---
+++ assets/js/modules/copy.js
@@ -0,0 +1,31 @@
+function ready() {
+
+  document.querySelectorAll('[data-markdown]').forEach(markdown => {
+
+    markdown.addEventListener('click', () => {
+
+      var text = fetch(markdown.dataset.markdown).then(response => response.text());
+      var blob = text.then(value => new Blob([value], { type: 'text/plain' }));
+
+      var written = window.ClipboardItem
+        ? navigator.clipboard.write([new ClipboardItem({ 'text/plain': blob })])
+        : text.then(value => navigator.clipboard.writeText(value));
+
+      written.then(() => flash(markdown));
+
+    });
+
+  });
+
+  function flash(button) {
+
+    clearTimeout(button.copied);
+
+    button.setAttribute('data-copied', '');
+    button.copied = setTimeout(() => button.removeAttribute('data-copied'), 2000);
+
+  }
+
+}
+
+export { ready };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+import * as copy from './modules/copy.js';
 import * as search from './modules/search.js';
 import * as toc from './modules/toc.js';
 import * as workstations from './modules/workstations.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  copy.ready();
 
   search.ready();
 
